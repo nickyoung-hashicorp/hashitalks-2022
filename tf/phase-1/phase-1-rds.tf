01f4844c62b5ba3727811bd4d7b353f9ff2cc6da
nickyoung-hashicorp <nyoung@hashicorp.com>
2022-02-04 15:20:40 -0800

    updated instructions

---
+++ tf/phase-1/phase-1-rds.tf
@@ -0,0 +1,11 @@
+resource "aws_db_instance" "vault" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "mysqldb"
+  username             = "hashitalks2022"
+  password             = "migrateVault!"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
