b741d7d7175f490d52f14c8552f899f344df8a94
nickyoung-hashicorp <nyoung@hashicorp.com>
2022-02-08 15:57:01 -0800

    instructions update

--- tf/files/vault_ent/test_vault_ent.sh
+++ tf/files/vault_ent/test_vault_ent.sh
@@ -3,22 +3,43 @@
 vault login $(jq -r .root_token < vault_init.json)
 sleep 2
 
-tput setaf 6; echo "RETRIEVE KEY-VALUE"
+tput setaf 2; echo "RETRIEVE KEY-VALUE"
+tput setaf 2; echo "=================="
+sleep 1
 vault kv get kv/hashitalks-secret
 sleep 2
 
-tput setaf 6; echo "DECRYPT CIPHERTEXT"
+tput setaf 2; echo "DECRYPT CIPHERTEXT"
+tput setaf 2; echo "=================="
+sleep 1
 vault write -field=plaintext transit/decrypt/hashitalks ciphertext=$(cat ciphertext.txt | jq -r '.data.ciphertext') | base64 --decode
 sleep 2
 
-tput setaf 6; echo "GENERATE CERTIFICATE"
+tput setaf 2; echo "GENERATE CERTIFICATE"
+tput setaf 2; echo "===================="
+sleep 1
 vault write pki/issue/hashitalks-dot-com \
     common_name=www.hashitalks.com
 sleep 2
 
-tput setaf 6; echo "LOOKUP FIRST MYSQL CREDENTIAL LEASE"
+tput setaf 2; echo "LOOKUP FIRST MYSQL CREDENTIAL LEASE"
+tput setaf 2; echo "==================================="
+sleep 1
 vault write sys/leases/lookup lease_id=$(cat lease_id.txt)
 sleep 2
 
-tput setaf 6; echo "GENERATE DYNAMIC MYSQL CREDENTIALS"
+tput setaf 2; echo "GENERATE DYNAMIC MYSQL CREDENTIALS"
+tput setaf 2; echo "=================================="
+sleep 1
 vault read database/creds/hashitalks-role
+
+tput setaf 2; echo "LIST VAULT POLICIES"
+tput setaf 2; echo "==================="
+sleep 1
+vault policy list
+sleep 2
+
+tput setaf 2; echo "TEST VAULT POLICY"
+tput setaf 2; echo "================="
+sleep 1
+export VAULT_TOKEN="$(vault token create -field token -policy=hashitalks-policy)"
--- tf/files/vault_oss/config_vault.sh
+++ tf/files/vault_oss/config_vault.sh
@@ -1,21 +1,31 @@
 #!/usr/bin/env bash
 
-echo "Configuring Key-Value Secrets Engine"
+tput setaf 7; echo "CONFIGURING KEY-VALUE SECRETS ENGINE"
+tput setaf 7; echo "===================================="
 sleep 1
 vault secrets enable -version=2 kv
 vault kv put kv/hashitalks-secret year=2022 date=02-17-2022
+vault kv put kv/hashitalks-speaker name="Nick Young"
 vault kv get kv/hashitalks-secret
 
-echo "Configuring Transit Secrets Engine"
+tput setaf 7; echo "CONFIGURING TRANSIT SECRETS ENGINE"
+tput setaf 7; echo "=================================="
 sleep 1
 vault secrets enable transit
 vault write -f transit/keys/hashitalks
 vault write transit/encrypt/hashitalks plaintext=$(base64 <<< "Welcome to HashiTalks 2022!")
 vault write transit/encrypt/hashitalks plaintext=$(base64 <<< "Welcome to HashiTalks 2022!") -format=json > ciphertext.txt
 cat ciphertext.txt | jq -r '.data.ciphertext'
+tput setaf 7; echo "Encrypting plaintext \"Welcome to HashiTalks 2022!\""
+sleep 1
+tput setaf 7; echo "into ciphertext $(cat ciphertext.txt)"
+sleep 1
+tput setup 7; echo "And decrypting ciphertext back to plaintext"
 vault write -field=plaintext transit/decrypt/hashitalks ciphertext=$(cat ciphertext.txt | jq -r '.data.ciphertext') | base64 --decode
+sleep 2
 
-echo "Configuring PKI Secrets Engine"
+tput setaf 7; echo "CONFIGURING PKI SECRETS ENGINE"
+tput setaf 7; echo "=============================="
 sleep 1
 vault secrets enable pki
 vault write pki/root/generate/internal \
@@ -31,7 +41,8 @@ vault write pki/roles/hashitalks-dot-com \
 vault write pki/issue/hashitalks-dot-com \
     common_name=www.hashitalks.com
 
-echo "Configuring Database Secrets Engine"
+tput setaf 7; echo "CONFIGURING DATABASE SECRETS ENGINE"
+tput setaf 7; echo "==================================="
 sleep 1
 vault secrets enable database
 vault write database/config/my-mysql-database \
@@ -45,5 +56,51 @@ vault write database/roles/hashitalks-role \
     creation_statements="CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}';GRANT SELECT ON *.* TO '{{name}}'@'%';" \
     default_ttl="8h" \
     max_ttl="24h"
-echo "Generate MySQL Database Credential and store lease ID"
+tput setaf 7; echo "GENERATE MYSQL DATABASE CREDENTIALS AND STORE LEASE ID"
+tput setaf 7; echo "======================================================"
+sleep 1
 vault read database/creds/hashitalks-role -format=json | jq -r '.lease_id' > lease_id.txt
+sleep 1
+tput setaf 7; echo "Lease ID = $(cat lease_id.txt)"
+sleep 1
+
+tput setaf 7; echo "CREATE VAULT POLICY"
+tput setaf 7; echo "==================="
+sleep 1
+vault policy write hashitalks-policy - << EOF
+path "kv/data/*" {
+  capabilities = ["create", "update","read"]
+}
+
+path "kv/data/hashitalks-speaker" {
+  capabilities = ["deny"]
+}
+EOF
+sleep 1
+tput setaf 7; echo "CREAT USERPASS AUTH METHOD WITH \"hashitalks-policy\""
+tput setaf 7; echo "====================================================="
+sleep 1
+vault auth enable userpass
+vault write auth/userpass/users/nickyoung \
+    password=hashitalks \
+    policies=hashitalks-policy
+sleep 1
+tput setaf 7; echo "LOGIN WITH USERPASS AUTH METHOD"
+tput setaf 7; echo "==============================="
+sleep 1
+vault login -method=userpass \
+    username=nickyoung \
+    password=hashitalks
+sleep 1
+tput setaf 7; echo "TEST SUCCESSFUL KV GET"
+tput setaf 7; echo "======================"
+sleep 1
+vault kv get kv/hashitalks-secret
+sleep 1
+tput setaf 7; echo "TEST FAILING KV GET"
+tput setaf 7; echo "==================="
+sleep 1
+vault kv get kv/hashitalks-speaker
+sleep 2
+
+
