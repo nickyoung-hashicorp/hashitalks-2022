187949d90808430e4d9a85cdf953aca599b16955
nickyoung-hashicorp <nyoung@hashicorp.com>
2022-02-09 20:19:44 -0800

    instructions update

--- tf/save_output.sh
+++ tf/save_output.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 terraform output -json > output.txt
-scp -i privateKey.pem output.txt privateKey.pem ubuntu@$(cat output.txt | jq -r '.vault_ip.value'):~
 echo "export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" >> access_key.txt
 echo "export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" >> secret_key.txt
-scp -i privateKey.pem output.txt privateKey.pem access_key.txt secret_key.txt ubuntu@$(cat output.txt | jq -r '.vault_ent_ip.value'):~
+scp -i privateKey.pem output.txt access_key.txt secret_key.txt privateKey.pem ubuntu@$(cat output.txt | jq -r '.vault_ip.value'):~
+scp -i privateKey.pem output.txt access_key.txt secret_key.txt privateKey.pem ubuntu@$(cat output.txt | jq -r '.vault_ent_ip.value'):~
 scp -i privateKey.pem output.txt access_key.txt secret_key.txt ubuntu@$(cat output.txt | jq -r '.vault_hsm_ip.value'):~
-scp -i privateKey.pem output.txt privateKey.pem access_key.txt secret_key.txt ubuntu@$(cat output.txt | jq -r '.vault_ip.value'):~
